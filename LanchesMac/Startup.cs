5492c365e38f9685518e3db04cd75219217e0e20
Márcio Lopes <mlsmarciogm@gmail.com>
2025-07-02 22:26:51 -0300

    Configurações iniciais, adicionando a classe Startup, aderindo a configuração no padrão Startup.

---
+++ LanchesMac/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace LanchesMac
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add serv
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Serviço de Autorização necessário para UseAuthorization
+            services.AddAuthorization();
+        }
+
+        // This method gets called by the runtime. Use this method to configur
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
