b05b52dc89012734ed4934550bf55d752bbfe185
Márcio Lopes <mlsmarciogm@gmail.com>
2025-07-23 22:40:53 -0300

    Implementando o padrão Repository Categorias

---
+++ LanchesMac/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+
+    }
+}
