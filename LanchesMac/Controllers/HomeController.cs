45fc65795d604d0af44be43243d76a2c1111c8b7
Márcio Lopes <mlsmarciogm@gmail.com>
2025-08-05 21:16:39 -0300

    Criando uma View Component para exibir detalhes do carrinho. Foram implementados os passos: 1) Criar a classe do View Component; criar a pasta Components no projeto, criar a classe CarrinhoCompraResumo que deriva de ViewComponent e definir o código do método Invoke(). 2) Criar a view do View Component; criar a pasta Components dentro da pasta /Views/Shared, criar a pasta CarrinhoCompraResumo dentro da pasta Components e criar a view Default.cshtml nesta pasta CarrinhoCompraResumo

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,15 +1,30 @@
 ﻿using System.Diagnostics;
 using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            this._lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            TempData["Nome"] = "Márcio";
-            return View();
+            //TempData["Nome"] = "Márcio";
+
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
