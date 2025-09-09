4688b1bc0349342619f194c8f93710a32bd301ca
Márcio Lopes <mlsmarciogm@gmail.com>
2025-09-09 11:20:28 -0300

    Impelementando o controller PedidoController e a view Checkout (formulário com os dados do pedido)

---
+++ LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _edidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository edidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _edidoRepository = edidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+
+    }
+}
