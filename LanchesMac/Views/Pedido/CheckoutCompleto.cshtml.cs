54ff5691a2ec4313aed899b44c2cb9f6d7f27da3
Márcio Lopes <mlsmarciogm@gmail.com>
2025-09-09 22:24:34 -0300

    Criando a View CheckoutCompleto - Exibindo um resumo do pedido

---
+++ LanchesMac/Views/Pedido/CheckoutCompleto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LanchesMac.Views.Pedido
+{
+    public class CheckoutCompletoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
